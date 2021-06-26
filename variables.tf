a604059d6621e5dfa037699c408818dcd2053809
Matheus Scarpato Fidelis <matheus@nanoshots.com.br>
2021-06-26 19:36:27 -0300

    initial commit

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "cluster_name" {
+    default = "msk-cluster"
+}
+
+variable "aws_region" {
+    default = "us-east-1"
+}
+
+variable "vpc_id" {
+    default = "vpc-ba8b92c1"
+}
+
+variable "vpc_subnets" {
+    default = [
+        "subnet-29954875",
+        "subnet-c832eeaf",
+        "subnet-23a9760d"
+    ]
+}
+
+variable "kafka_version" {
+    default = "2.6.1"
+}
+
+variable "brokers_count" {
+    default = 3
+}
+
+variable "broker_instance_type" {
+    default = "kafka.t3.small"
+}
+
+variable "broker_ebs_volume_size" {
+    default = 100
+}
+
+variable "open_monitoring" {
+    default = true
+}
+
+variable "tags" {
+    default = {
+        foo = "var"
+    }
+}
